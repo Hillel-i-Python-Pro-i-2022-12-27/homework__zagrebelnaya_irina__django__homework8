ca6a487f168b87082fa340fe01bd2495c2095f95
izagrebelnaya <izagrebelnaya@atncorp.com>
2023-02-22 16:39:56 +0200

    HW 10. Django. Contacts and CRUD.

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -5,9 +5,10 @@ from apps.contacts import views
 app_name = "contacts"
 
 urlpatterns = [
-    path('', views.list_of_contacts),
-    path('create/', views.create, name='create'),
-    path('edit/<int:pk>', views.edit, name='edit'),
-    path("info/", views.info, name="info"),
+    path("", views.list_of_contacts, name="root"),
+    path("list/", views.list_of_contacts, name="list"),
+    path("create/", views.create, name="create"),
+    path("edit/<int:pk>", views.edit, name="edit"),
+    path("info/<int:pk>", views.info, name="info"),
     path("delete/", views.delete, name="delete"),
 ]
