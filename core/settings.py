d47be2b2a03f4f12427d8fe8a90015559c5d8680
izagrebelnaya <izagrebelnaya@atncorp.com>
2023-02-23 11:29:40 +0200

    HW 10. Fix

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-LOCAL_APPS = ["apps.base", "apps.contacts", "apps.users"]
+LOCAL_APPS = ["apps.base", "apps.contacts"]
 
 THIRD_PARTY_APPS = [
     "crispy_forms",
